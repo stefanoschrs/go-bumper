d169be3e538adb05952806ec3ed1ee983fa186ce
Theo <theo.despoudis@teckro.com>
2017-06-26 23:46:43 +0100

    Slightly more readable code

--- go-bumper.go
+++ go-bumper.go
@@ -1,58 +1,48 @@
 package main
 
 import (
-	"fmt"
-	"regexp"
-	"io/ioutil"
-	"strings"
-	"strconv"
 	"flag"
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
-var bumpType = 0
-var supportedFiles = []string{
-	"package.json",
-	"config.xml",
+var supportedFiles = map[string]string{
+	"package.json": "package.json",
+	"config.xml":   "config.xml",
 }
 
-func _contains(s []string, e string) bool {
-	for _, a := range s {
-		if a == e {
-			return true
-		}
-	}
-	return false
-}
-
-func _versionBumpHelper(version string, index int) string {
+func versionBumpHelper(version string, index int) string {
 	versionParts := strings.Split(version, ".")
 
 	if n, err := strconv.ParseInt(versionParts[index], 10, 8); err == nil {
-		versionParts[index] = strconv.FormatInt(n + 1, 10)
+		versionParts[index] = strconv.FormatInt(n+1, 10)
 	}
 
 	return strings.Join(versionParts, ".")
 }
 
 func versionPatch(version string) string {
-	return _versionBumpHelper(version, 2)
+	return versionBumpHelper(version, 2)
 }
 
 func versionMinor(version string) string {
-	return _versionBumpHelper(version, 1)
+	return versionBumpHelper(version, 1)
 }
 
 func versionMajor(version string) string {
-	return _versionBumpHelper(version, 0)
+	return versionBumpHelper(version, 0)
 }
 
-func bumpConfigXml()  {
-	data, err := ioutil.ReadFile("config.xml")
+func bumpFile(bumpType int, fileName string, pattern string) {
+	data, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		panic(err)
 	}
 
-	re := regexp.MustCompile(`<widget.*version="([0-9]+\.[0-9]+\.[0-9]+)"`)
+	re := regexp.MustCompile(pattern)
 	version := re.FindStringSubmatch(string(data))[1]
 	fmt.Printf("Current version: %s\n", version)
 
@@ -73,76 +63,54 @@ func bumpConfigXml()  {
 	re = regexp.MustCompile(version)
 	newData := re.ReplaceAllString(string(data), newVersion)
 
-	ioutil.WriteFile("config.xml", []byte(newData), 0644)
+	ioutil.WriteFile(fileName, []byte(newData), 0644)
 }
 
-func bumpPackageJson()  {
-	data, err := ioutil.ReadFile("package.json")
-	if err != nil {
-		panic(err)
-	}
-
-	re := regexp.MustCompile(`"version": "([0-9]+\.[0-9]+\.[0-9]+)"`)
-	version := re.FindStringSubmatch(string(data))[1]
-	fmt.Printf("Current version: %s\n", version)
-
-	var newVersion string
-	switch bumpType {
-	case 0:
-		newVersion = versionPatch(version)
-		break
-	case 1:
-		newVersion = versionMinor(version)
-		break
-	case 2:
-		newVersion = versionMajor(version)
-		break
-	}
-	fmt.Printf("New version: %s\n", newVersion)
-
-	re = regexp.MustCompile(version)
-	newData := re.ReplaceAllString(string(data), newVersion)
-
-	ioutil.WriteFile("package.json", []byte(newData), 0644)
-}
-
-func bump(name string)  {
+func bump(name string, bumpType int) {
 	switch name {
 	case "config.xml":
-		bumpConfigXml()
+		bumpFile(bumpType, "config.xml",
+			`<widget.*version="([0-9]+\.[0-9]+\.[0-9]+)"`)
 		break
 	case "package.json":
-		bumpPackageJson()
+		bumpFile(bumpType, "package.json",
+			`"version": "([0-9]+\.[0-9]+\.[0-9]+)"`)
 		break
 	}
 }
 
 func main() {
-	var patchFlag = flag.Bool("patch", false, "Patch Bump")
-	var minorFlag = flag.Bool("minor", false, "Minor Bump")
-	var majorFlag = flag.Bool("major", false, "Major Bump")
-
-	flag.Parse()
-	fmt.Printf("Patch: %t, Minor: %t, Major: %t\n", *patchFlag, *minorFlag, *majorFlag)
-	if *patchFlag {
-		bumpType = 0
-	}
-
-	if *minorFlag {
-		bumpType = 1
-	}
-
-	if *majorFlag {
-		bumpType = 2
-	}
+	bumpType := parseFlags()
 
 	files, _ := ioutil.ReadDir("./")
 	for _, f := range files {
 		name := f.Name()
 
-		if _contains(supportedFiles, name) {
+		if _, ok := supportedFiles[name]; ok {
 			fmt.Printf("Bumping %s\n", name)
-			bump(name)
+			bump(name, bumpType)
 		}
 	}
+}
+
+func parseFlags() (bumpType int) {
+	patchFlag := flag.Bool("patch", false, "Patch Bump")
+	minorFlag := flag.Bool("minor", false, "Minor Bump")
+	majorFlag := flag.Bool("major", false, "Major Bump")
+
+	flag.Parse()
+	fmt.Printf("Patch: %t, Minor: %t, Major: %t\n",
+		*patchFlag, *minorFlag, *majorFlag)
+
+	if *patchFlag {
+		bumpType = 0
+	} else if *minorFlag {
+		bumpType = 1
+	} else if *majorFlag {
+		bumpType = 2
+	} else {
+		bumpType = 0
+	}
+
+	return
 }
